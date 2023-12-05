66c9c5548a2b472fe01fd4cfe3d124d7e00cd1cb
Kallew Machado <kallewcarlos@gmail.com>
2023-12-05 14:47:22 -0300

    feat: add supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await FlutterConfig.loadEnvVariables();
+
+  await Supabase.initialize(
+    url: FlutterConfig.get('supabaseUrl'),
+    anonKey: FlutterConfig.get('supabaseAnonKey'),
+  );
+
+  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+}
