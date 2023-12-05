53df51c8854685d96beeb77bb1b935f5b2cbef2a
Kallew Machado <kallewcarlos@gmail.com>
2023-12-05 09:48:11 -0300

    feat: add theme app widget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,14 +1,18 @@
+import 'package:control_data/app/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Slidy',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      routerDelegate: Modular.routerDelegate,
-      routeInformationParser: Modular.routeInformationParser,
+      title: 'Control Data',
+      themeMode: ThemeMode.dark,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      routerConfig: Modular.routerConfig,
     );
   }
 }
