e20a909c647f32a81fe735a51e4a48bfe3f3660d
Kallew Machado <kallewcarlos@gmail.com>
2023-12-05 09:47:08 -0300

    feat: add theme

---
+++ lib/app/core/theme/theme.dart
@@ -0,0 +1,20 @@
+import 'package:control_data/app/core/theme/color_schemes.g.dart';
+import 'package:flutter/material.dart';
+
+ThemeData get lightTheme => ThemeData(
+      useMaterial3: true,
+      colorScheme: lightColorScheme,
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: darkColorScheme.primary,
+        foregroundColor: Colors.black,
+      ),
+    );
+
+ThemeData get darkTheme => ThemeData(
+      useMaterial3: true,
+      colorScheme: darkColorScheme,
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: darkColorScheme.primary,
+        foregroundColor: Colors.black,
+      ),
+    );
