d342da22a644b2c78a91f7a60984f3b56b5831bd
Kallew Machado <kallewcarlos@gmail.com>
2023-12-06 09:48:17 -0300

    feat: add form in authpage

--- lib/app/modules/auth/views/auth_page.dart
+++ lib/app/modules/auth/views/auth_page.dart
@@ -1,3 +1,4 @@
+import 'package:control_data/app/core/views/widgets/custom_textform_widget.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -8,8 +9,36 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      child: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Control Data"),
+            const SizedBox(height: 60),
+            CustomTextFormWidget(
+              controller: _emailController,
+            ),
+            const SizedBox(height: 10),
+            CustomTextFormWidget(
+              controller: _passwordController,
+              obscureText: true,
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.remove_red_eye_sharp),
+              ),
+            ),
+            const SizedBox(height: 30),
+            OutlinedButton(onPressed: () {}, child: const Text("Entrar"))
+          ],
+        ),
+      ),
+    );
   }
 }
