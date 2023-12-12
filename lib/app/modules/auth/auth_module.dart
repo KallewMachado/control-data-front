6858c286154d235f12e47c0f6a79b9f78d9a96b7
Kallew Machado <kallewcarlos@gmail.com>
2023-12-12 16:52:10 -0300

    feat: add RegisterUserPage in Auth Module

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,3 +1,5 @@
+import 'package:control_data/app/core/repositories/user_repository.dart';
+import 'package:control_data/app/core/views/register_user_page.dart';
 import 'package:control_data/app/modules/auth/views/auth_page.dart';
 import 'package:control_data/app/modules/auth/views/auth_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -6,12 +8,14 @@ class AuthModule extends Module {
   @override
   void binds(i) {
     super.binds(i);
-    i.addLazySingleton(AuthStore.new);
+    i.addLazySingleton<AuthStore>(AuthStore.new);
+    i.add<UserRepository>(UserRepositoryImpl.new);
   }
 
   @override
   void routes(r) {
     super.routes(r);
     r.child(Modular.initialRoute, child: (_) => const AuthPage());
+    r.child('/register', child: (_) => const RegisterUserPage());
   }
 }
