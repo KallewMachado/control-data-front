1bbb8f72ae56a2148306189980871417354bcda4
Kallew Machado <kallewcarlos@gmail.com>
2023-12-13 10:22:33 -0300

    feat: add fix Inject Dependency

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,9 +1,13 @@
 import 'package:control_data/app/modules/auth/auth_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'modules/auth/views/auth_store.dart';
+
 class AppModule extends Module {
   @override
-  void binds(i) {}
+  void binds(i) {
+    i.addLazySingleton<AuthStore>(AuthStore.new);
+  }
 
   @override
   void routes(r) {
