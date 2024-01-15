eabad397f793596513977a5de2424495d71d84f5
Kallew Machado <kallewcarlos@gmail.com>
2024-01-15 17:46:39 -0300

    feat: fix form

--- lib/app/core/views/register_user_page.dart
+++ lib/app/core/views/register_user_page.dart
@@ -1,8 +1,10 @@
 import 'package:control_data/app/core/views/widgets/custom_textform_widget.dart';
 import 'package:control_data/app/modules/auth/views/auth_store.dart';
+import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../model/auth_model.dart';
 
 class RegisterUserPage extends StatefulWidget {
@@ -16,6 +18,7 @@ class _RegisterUserPageState extends State<RegisterUserPage> {
   final _authStore = Modular.get<AuthStore>();
   final _formKey = GlobalKey<FormState>();
   final _nameController = TextEditingController();
+  final _cpfController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
@@ -25,6 +28,8 @@ class _RegisterUserPageState extends State<RegisterUserPage> {
   final _districtController = TextEditingController();
   final _complementController = TextEditingController();
   final _dateBirthController = TextEditingController();
+
+  final isRequiredField = 'Este campo é obrigatório!';
 
   @override
   void dispose() {
@@ -45,7 +50,9 @@ class _RegisterUserPageState extends State<RegisterUserPage> {
   @override
   Widget build(BuildContext context) {
     double spacing = 10;
+    ThemeData theme = Theme.of(context);
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
@@ -54,35 +61,109 @@ class _RegisterUserPageState extends State<RegisterUserPage> {
               key: _formKey,
               child: Column(
                 children: [
+                  Text(
+                    'Novo Usuario',
+                    style: theme.textTheme.headlineMedium
+                        ?.copyWith(color: theme.colorScheme.primary),
+                  ),
                   const SizedBox(height: 50),
                   CustomTextFormWidget(
                     controller: _nameController,
                     label: const Text('Nome'),
+                    validator: (name) {
+                      if (name == null || name.isEmpty) {
+                        return isRequiredField;
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(height: spacing),
+                  CustomTextFormWidget(
+                    controller: _cpfController,
+                    label: const Text('CPF'),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      CpfInputFormatter()
+                    ],
+                    validator: (cpf) {
+                      if (cpf == null || cpf.isEmpty) {
+                        return isRequiredField;
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
                   SizedBox(height: spacing),
                   CustomTextFormWidget(
                     controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
                     label: const Text('E-mail'),
+                    validator: (email) {
+                      if (email == null || email.isEmpty) {
+                        return isRequiredField;
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
                   SizedBox(height: spacing),
                   CustomTextFormWidget(
                     controller: _passwordController,
+                    obscureText: true,
                     label: const Text('Senha'),
+                    validator: (password) {
+                      if (password == null || password.isEmpty) {
+                        return isRequiredField;
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
                   SizedBox(height: spacing),
                   CustomTextFormWidget(
                     controller: _confirmPasswordController,
+                    obscureText: true,
                     label: const Text('Confirmar Senha'),
+                    validator: (confirmPass) {
+                      if (confirmPass == null || confirmPass.isEmpty) {
+                        return isRequiredField;
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
                   SizedBox(height: spacing),
                   CustomTextFormWidget(
                     controller: _foneController,
                     label: const Text('Celular'),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      TelefoneInputFormatter()
+                    ],
+                    validator: (fone) {
+                      if (fone == null || fone.isEmpty) {
+                        return isRequiredField;
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
                   SizedBox(height: spacing),
                   CustomTextFormWidget(
+                    isDate: true,
                     controller: _dateBirthController,
+                    readOnly: true,
                     label: const Text('Data de nascimento'),
+                    validator: (dateBirth) {
+                      if (dateBirth == null || dateBirth.isEmpty) {
+                        return isRequiredField;
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
                   SizedBox(height: spacing),
                   Row(
@@ -92,6 +173,13 @@ class _RegisterUserPageState extends State<RegisterUserPage> {
                         child: CustomTextFormWidget(
                           controller: _endressController,
                           label: const Text('Endereço'),
+                          validator: (endress) {
+                            if (endress == null || endress.isEmpty) {
+                              return isRequiredField;
+                            } else {
+                              return null;
+                            }
+                          },
                         ),
                       ),
                       const Spacer(),
@@ -100,6 +188,13 @@ class _RegisterUserPageState extends State<RegisterUserPage> {
                         child: CustomTextFormWidget(
                           controller: _numController,
                           label: const Text('Numero'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return isRequiredField;
+                            } else {
+                              return null;
+                            }
+                          },
                         ),
                       ),
                     ],
@@ -108,42 +203,67 @@ class _RegisterUserPageState extends State<RegisterUserPage> {
                   CustomTextFormWidget(
                     controller: _districtController,
                     label: const Text('Bairro'),
+                    validator: (district) {
+                      if (district == null || district.isEmpty) {
+                        return isRequiredField;
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
                   SizedBox(height: spacing),
                   CustomTextFormWidget(
                     controller: _complementController,
                     label: const Text('Complemento'),
+                    validator: (complement) {
+                      if (complement == null || complement.isEmpty) {
+                        return isRequiredField;
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
                   const SizedBox(height: 40),
                   OutlinedButton(
                     onPressed: () async {
                       if (_formKey.currentState!.validate()) {
-                        AuthModel auth = AuthModel(
-                          id: '',
-                          email: 'kallewcarlos@gmail.com',
-                          password: '654321',
-                        );
+                        try {
+                          AuthModel auth = AuthModel(
+                            id: '',
+                            email: _emailController.text.trim(),
+                            password: _passwordController.text.trim(),
+                          );
 
-                        DateTime date =
-                            DateTime.parse(_dateBirthController.text.trim());
-                        String dateBirth = date.toString().split(' ')[0];
+                          DateTime date = DateTime.parse(_dateBirthController
+                              .text
+                              .replaceAll('/', '-')
+                              .trim());
+                          String dateBirth = date.toString().split(' ')[0];
 
-                        AuthModel result = await _authStore.createUser(auth);
+                          AuthModel result = await _authStore.createUser(auth);
 
-                        Map<String, dynamic> json = {
-                          "id": result.id,
-                          "name": _nameController.text.trim(),
-                          "email": result.email,
-                          "date_birth": dateBirth,
-                          "fone": _foneController.text.trim(),
-                          "street": _endressController.text.trim(),
-                          "num": _numController.text.trim(),
-                          "district": _districtController.text.trim(),
-                          "complement": _complementController.text.trim(),
-                          "user_created": result.id,
-                        };
+                          Map<String, dynamic> json = {
+                            "id": result.id,
+                            "name": _nameController.text.trim(),
+                            "cpf": _cpfController.text.trim(),
+                            "email": result.email,
+                            "date_birth": dateBirth,
+                            "fone": _foneController.text.trim(),
+                            "street": _endressController.text.trim(),
+                            "num": _numController.text.trim(),
+                            "district": _districtController.text.trim(),
+                            "complement": _complementController.text.trim(),
+                            "user_created": result.id,
+                          };
 
-                        await _authStore.newUser(json);
+                          await _authStore.newUser(json);
+
+                          Modular.to.pop();
+                        } on AuthException catch (e) {
+                          print(e.message);
+                        } on PostgrestException catch (e) {
+                          print(e.message);
+                        }
                       }
                     },
                     child: const Text('Registrar'),
@@ -153,7 +273,8 @@ class _RegisterUserPageState extends State<RegisterUserPage> {
                       Modular.to.pop();
                     },
                     child: const Text('Cancelar'),
-                  )
+                  ),
+                  const SizedBox(height: 40),
                 ],
               ),
             ),
