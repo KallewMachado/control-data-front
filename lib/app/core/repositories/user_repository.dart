653f74b80de319edcda8b5f02e5738ce5bd25abf
Kallew Machado <kallewcarlos@gmail.com>
2024-01-15 17:46:02 -0300

    feat: fix spacing

--- lib/app/core/repositories/user_repository.dart
+++ lib/app/core/repositories/user_repository.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:control_data/app/core/model/auth_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:control_data/app/core/model/user_model.dart';
