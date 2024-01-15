1a6a8352f48d41c76beb4fa5f6fceeec6e616b8a
Kallew Machado <kallewcarlos@gmail.com>
2024-01-15 17:47:13 -0300

    feat: add module home

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -4,6 +4,8 @@ import 'package:control_data/app/modules/auth/views/auth_page.dart';
 import 'package:control_data/app/modules/auth/views/auth_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../home/home_module.dart';
+
 class AuthModule extends Module {
   @override
   void binds(i) {
@@ -17,5 +19,6 @@ class AuthModule extends Module {
     super.routes(r);
     r.child(Modular.initialRoute, child: (_) => const AuthPage());
     r.child('/register', child: (_) => const RegisterUserPage());
+    r.module('/home', module: HomeModule());
   }
 }
