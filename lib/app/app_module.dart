4ba81db48bca5acd93b460da343c6a62c89c1f37
Kallew Machado <kallewcarlos@gmail.com>
2024-01-15 17:45:05 -0300

    feat: add UserRepository inject dependecy

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,12 +1,14 @@
 import 'package:control_data/app/modules/auth/auth_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'core/repositories/user_repository.dart';
 import 'modules/auth/views/auth_store.dart';
 
 class AppModule extends Module {
   @override
   void binds(i) {
     i.addLazySingleton<AuthStore>(AuthStore.new);
+    i.add<UserRepository>(UserRepositoryImpl.new);
   }
 
   @override
