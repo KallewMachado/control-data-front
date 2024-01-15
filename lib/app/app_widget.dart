a5e31e872e8a6cf2e306323a079bbb9497d650e9
Kallew Machado <kallewcarlos@gmail.com>
2024-01-15 17:45:18 -0300

    feat: add Localizations

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:control_data/app/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -8,6 +9,11 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       title: 'Control Data',
       themeMode: ThemeMode.dark,
       theme: lightTheme,
