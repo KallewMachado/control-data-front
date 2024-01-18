5b6c28e9a23a29465981fed346712aa0f856f723
Kallew Machado <kallewcarlos@gmail.com>
2024-01-18 10:30:24 -0300

    feat: fix home module

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,15 +1,34 @@
 import 'package:control_data/app/modules/home/views/home_page.dart';
 import 'package:control_data/app/modules/home/views/home_store.dart';
+import 'package:control_data/app/modules/initial/initial_store.dart';
+import 'package:control_data/app/modules/users/users_store.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import '../configurations/configurations_page.dart';
+import '../initial/initial_page.dart';
+import '../users/users_page.dart';
 
 class HomeModule extends Module {
   @override
   void binds(i) {
     i.addLazySingleton(HomeStore.new);
+    i.addLazySingleton(InitialStore.new);
+    i.addLazySingleton(UsersStore.new);
   }
 
   @override
   void routes(r) {
-    r.child(Modular.initialRoute, child: (_) => const HomePage());
+    r.child(
+      Modular.initialRoute,
+      child: (_) => const HomePage(),
+      children: [
+        ChildRoute('/initial', child: (_) => const InitialPage()),
+        ChildRoute('/voters', child: (context) => UsersPage()),
+        ChildRoute('/config',
+            child: (context) =>
+                const ConfigurationsPage(title: 'config', color: Colors.green)),
+      ],
+    );
   }
 }
