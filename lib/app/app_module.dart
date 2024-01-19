dda16528a205e05970da4ddea36e77f52e8cf6c6
Kallew Machado <kallewcarlos@gmail.com>
2024-01-19 11:46:43 -0300

    feat: fix AppModule

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,18 +1,16 @@
-import 'package:control_data/app/modules/auth/auth_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import 'core/repositories/user_repository.dart';
-import 'modules/auth/views/auth_store.dart';
+import 'core/store/app_store.dart';
+import 'modules/home/home_module.dart';
 
 class AppModule extends Module {
   @override
   void binds(i) {
-    i.addLazySingleton<AuthStore>(AuthStore.new);
-    i.add<UserRepository>(UserRepositoryImpl.new);
+    i.addLazySingleton<AppStore>(AppStore.new);
   }
 
   @override
   void routes(r) {
-    r.module('/', module: AuthModule());
+    r.module(Modular.initialRoute, module: HomeModule());
   }
 }
