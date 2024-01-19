674e8055a6bfe1645ea63c6f706444f6e35aea60
Kallew Machado <kallewcarlos@gmail.com>
2024-01-19 11:47:35 -0300

    feat: fix AuthModule

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,5 +1,5 @@
 import 'package:control_data/app/core/repositories/user_repository.dart';
-import 'package:control_data/app/core/views/register_user_page.dart';
+import 'package:control_data/app/core/views/register_new_user_page.dart';
 import 'package:control_data/app/modules/auth/views/auth_page.dart';
 import 'package:control_data/app/modules/auth/views/auth_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -18,7 +18,7 @@ class AuthModule extends Module {
   void routes(r) {
     super.routes(r);
     r.child(Modular.initialRoute, child: (_) => const AuthPage());
-    r.child('/register', child: (_) => const RegisterUserPage());
+    r.child('/register', child: (_) => const RegisterNewUserPage());
     r.module('/home', module: HomeModule());
   }
 }
