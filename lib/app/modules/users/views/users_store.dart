481f3ac53ce252a10be06e6fd563ec922540dbf8
Kallew Machado <kallewcarlos@gmail.com>
2024-01-23 12:01:45 -0300

    feat: add Users Store

--- lib/app/modules/users/views/users_store.dart
+++ lib/app/modules/users/views/users_store.dart
@@ -1,4 +1,7 @@
+import 'package:control_data/app/core/repositories/user_repository.dart';
+import 'package:control_data/app/core/utils/hive_config.dart';
 import 'package:mobx/mobx.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../core/model/user_model.dart';
 
@@ -7,6 +10,60 @@ part 'users_store.g.dart';
 class UsersStore = UsersStoreBase with _$UsersStore;
 
 abstract class UsersStoreBase with Store {
+  final UserRepository _repository;
+  final hive = HiveConfig();
+
   @observable
-  List<UserModel> usersList = [];
+  UserModel user = UserModel(
+    id: '',
+    name: 'name',
+    email: 'email',
+    dateBirth: DateTime(1997, 09, 18),
+    fone: 'fone',
+    street: 'street',
+    num: 'num',
+    district: 'district',
+    complement: 'complement',
+    userCreated: 'userCreated',
+    demands: [],
+    cpf: 'cpf',
+  );
+
+  ObservableList<UserModel> usersList = ObservableList<UserModel>();
+
+  UsersStoreBase(this._repository) {
+    initUser();
+  }
+
+  initUser() {
+    var userbox = hive.userBox.values.first;
+
+    user = UserModel(
+      id: userbox.id,
+      name: userbox.name,
+      email: userbox.email,
+      dateBirth: userbox.dateBirth,
+      fone: userbox.fone,
+      street: userbox.street,
+      num: userbox.num,
+      district: userbox.district,
+      complement: userbox.complement ?? '',
+      userCreated: userbox.userCreated,
+      demands: userbox.demands ?? [],
+      cpf: userbox.cpf,
+    );
+  }
+
+  @action
+  Future<void> getAllUsers() async {
+    try {
+      var users = await _repository.getAllUsers();
+      usersList.clear();
+      usersList.addAll(users);
+    } on PostgrestException catch (e) {
+      print(e.message);
+    } catch (e) {
+      print(e);
+    }
+  }
 }
--- lib/app/modules/users/views/users_store.g.dart
+++ lib/app/modules/users/views/users_store.g.dart
@@ -9,26 +9,33 @@ part of 'users_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$UsersStore on UsersStoreBase, Store {
-  late final _$usersListAtom =
-      Atom(name: 'UsersStoreBase.usersList', context: context);
+  late final _$userAtom = Atom(name: 'UsersStoreBase.user', context: context);
 
   @override
-  List<UserModel> get usersList {
-    _$usersListAtom.reportRead();
-    return super.usersList;
+  UserModel get user {
+    _$userAtom.reportRead();
+    return super.user;
   }
 
   @override
-  set usersList(List<UserModel> value) {
-    _$usersListAtom.reportWrite(value, super.usersList, () {
-      super.usersList = value;
+  set user(UserModel value) {
+    _$userAtom.reportWrite(value, super.user, () {
+      super.user = value;
     });
+  }
+
+  late final _$getAllUsersAsyncAction =
+      AsyncAction('UsersStoreBase.getAllUsers', context: context);
+
+  @override
+  Future<void> getAllUsers() {
+    return _$getAllUsersAsyncAction.run(() => super.getAllUsers());
   }
 
   @override
   String toString() {
     return '''
-usersList: ${usersList}
+user: ${user}
     ''';
   }
 }
