21431552769426300a589b30a8a1784bccfb2635
Kallew Machado <kallewcarlos@gmail.com>
2024-01-23 14:21:34 -0300

    feat: add UsersStore

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,5 +1,6 @@
 import 'package:control_data/app/modules/auth/auth_module.dart';
 import 'package:control_data/app/modules/auth/views/auth_store.dart';
+import 'package:control_data/app/modules/users/views/users_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'core/repositories/user_repository.dart';
@@ -9,6 +10,7 @@ class AppModule extends Module {
   @override
   void binds(i) {
     i.addLazySingleton<AppStore>(AppStore.new);
+    i.addLazySingleton<UsersStore>(UsersStore.new);
     i.addLazySingleton<AuthStore>(AuthStore.new);
     i.add<UserRepository>(UserRepositoryImpl.new);
   }
