2c36bb72ac9fe62c79937a032b6430c7cb7e97be
Kallew Machado <kallewcarlos@gmail.com>
2024-01-23 12:02:05 -0300

    feat: data in Card User

--- lib/app/modules/users/views/widgets/card_user_widget.dart
+++ lib/app/modules/users/views/widgets/card_user_widget.dart
@@ -1,19 +1,27 @@
+import 'package:control_data/app/modules/users/views/users_store.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../../core/model/user_model.dart';
 
 class CardUserWidget extends StatelessWidget {
-  const CardUserWidget({super.key});
+  const CardUserWidget({super.key, required this.user, required this.store});
+  final UserModel user;
+  final UsersStore store;
 
   @override
   Widget build(BuildContext context) {
-    return const ListTile(
-      shape: OutlineInputBorder(
+    DateTime dateBirth = user.dateBirth;
+    var date = DateFormat('dd/MM/yyyy');
+    return ListTile(
+      shape: const OutlineInputBorder(
         borderRadius: BorderRadius.all(Radius.circular(10)),
         borderSide: BorderSide(width: 1.5),
       ),
-      leading: Icon(Icons.person),
-      title: Text('Name User'),
-      subtitle: Text("date Nasc"),
-      trailing: Icon(
+      leading: const Icon(Icons.person),
+      title: Text(user.name),
+      subtitle: Text(date.format(dateBirth)),
+      trailing: const Icon(
         Icons.delete,
         color: Colors.red,
       ),
