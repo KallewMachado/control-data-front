d5c4985f42b2d96d17ddd92630c55b3f5af5bafb
Kallew Machado <kallewcarlos@gmail.com>
2024-01-23 12:01:25 -0300

    feat: add data in CardUserWidget

--- lib/app/modules/users/views/users_page.dart
+++ lib/app/modules/users/views/users_page.dart
@@ -1,36 +1,44 @@
 import 'package:control_data/app/modules/users/views/users_store.dart';
 import 'package:control_data/app/modules/users/views/widgets/card_user_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class UsersPage extends StatelessWidget {
-  UsersPage({Key? key}) : super(key: key);
+  UsersPage({super.key}) {
+    _store.getAllUsers();
+  }
   final _store = Modular.get<UsersStore>();
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-      child: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Adicionar novo usuario'),
-          ),
-          const SizedBox(height: 30),
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: 10,
-              itemBuilder: (ctx, index) {
-                return const Padding(
-                  padding: EdgeInsets.only(bottom: 8.0),
-                  child: CardUserWidget(),
-                );
+      child: Observer(builder: (context) {
+        return Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Modular.to.pushNamed('/home/register/user');
               },
+              child: const Text('Adicionar novo usuario'),
             ),
-          )
-        ],
-      ),
+            const SizedBox(height: 30),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _store.usersList.length,
+                itemBuilder: (ctx, index) {
+                  var user = _store.usersList[index];
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: CardUserWidget(user: user, store: _store),
+                  );
+                },
+              ),
+            )
+          ],
+        );
+      }),
     );
   }
 }
