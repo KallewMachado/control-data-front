54b1b2a87a476702dba0261fa03a2e7f6c979d23
Kallew Machado <kallewcarlos@gmail.com>
2024-01-25 14:48:54 -0300

    feat: add theme

--- lib/app/modules/users/views/widgets/card_user_widget.dart
+++ lib/app/modules/users/views/widgets/card_user_widget.dart
@@ -1,5 +1,6 @@
 import 'package:control_data/app/modules/users/views/users_store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/intl.dart';
 
 import '../../../../core/model/user_model.dart';
@@ -11,19 +12,31 @@ class CardUserWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context).colorScheme;
     DateTime dateBirth = user.dateBirth;
     var date = DateFormat('dd/MM/yyyy');
-    return ListTile(
-      shape: const OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        borderSide: BorderSide(width: 1.5),
-      ),
-      leading: const Icon(Icons.person),
-      title: Text(user.name),
-      subtitle: Text(date.format(dateBirth)),
-      trailing: const Icon(
-        Icons.delete,
-        color: Colors.red,
+    return Card(
+      child: ListTile(
+        onTap: () {
+          Modular.to.pushNamed('/home/demands/');
+        },
+        shape: OutlineInputBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          borderSide: BorderSide(
+            width: 1,
+            color: theme.outlineVariant,
+          ),
+        ),
+        leading: const Icon(Icons.person),
+        title: Text(user.name),
+        subtitle: Text(date.format(dateBirth)),
+        trailing: IconButton(
+          onPressed: () async {
+            await store.deleteUser(user.id);
+          },
+          icon: const Icon(Icons.delete),
+          color: Colors.red,
+        ),
       ),
     );
   }
