7d00b3832dccb44984b241bf8a5589b7a2dbe588
Kallew Machado <kallewcarlos@gmail.com>
2024-01-25 14:49:46 -0300

    feat: add module demands

---
+++ lib/app/modules/demands/demands_module.dart
@@ -0,0 +1,17 @@
+import 'package:control_data/app/modules/demands/views/demands_page.dart';
+import 'package:control_data/app/modules/users/views/users_store.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class DemandsModule extends Module {
+  @override
+  void binds(i) {
+    super.binds(i);
+    i.addLazySingleton<UsersStore>(UsersStore.new);
+  }
+
+  @override
+  void routes(r) {
+    super.routes(r);
+    r.child(Modular.initialRoute, child: (context) => const DemandsPage());
+  }
+}
