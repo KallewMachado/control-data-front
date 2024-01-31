237e0a42371bf988d48d1207581a819dcaab8005
Kallew Machado <kallewcarlos@gmail.com>
2024-01-31 15:05:52 -0300

    feat: fix store

--- lib/app/modules/initial/views/initial_store.dart
+++ lib/app/modules/initial/views/initial_store.dart
@@ -1,4 +1,5 @@
 import 'package:control_data/app/core/model/demands_model.dart';
+import 'package:control_data/app/core/repositories/demands_repository.dart';
 import 'package:mobx/mobx.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -7,14 +8,24 @@ part 'initial_store.g.dart';
 class InitialStore = InitialStoreBase with _$InitialStore;
 
 abstract class InitialStoreBase with Store {
-  @observable
-  List<DemandsModel> demandsList = [];
+  final DemandsRepository _repository;
 
-  Future<void> getDemands() async {
-    try {} on PostgrestException catch (e) {
+  ObservableList<DemandsModel> demandsList = ObservableList<DemandsModel>();
+
+  InitialStoreBase(this._repository);
+
+  Future<void> getAllDemands() async {
+    try {
+      var demands = await _repository.getAllDemands();
+
+      demands.sort((a, b) => b.solicitationDate.compareTo(a.solicitationDate));
+
+      demandsList.clear();
+      demandsList.addAll(demands);
+    } on PostgrestException catch (e) {
       print(e.message);
     } catch (e) {
-      print(e.toString());
+      print(e);
     }
   }
 }
