dd1ea3243e2d34c46595bef312cb7547a2952c66
Kallew Machado <kallewcarlos@gmail.com>
2024-01-31 15:04:16 -0300

    feat: remove getAllDemands

--- lib/app/modules/demands/views/demands_store.dart
+++ lib/app/modules/demands/views/demands_store.dart
@@ -12,7 +12,6 @@ class DemandsStore = DemandsStoreBase with _$DemandsStore;
 abstract class DemandsStoreBase with Store {
   final DemandsRepository _repository;
 
-  ObservableList<DemandsModel> demandsList = ObservableList<DemandsModel>();
   ObservableList<DemandsModel> demandsListByUser =
       ObservableList<DemandsModel>();
 
@@ -20,19 +19,6 @@ abstract class DemandsStoreBase with Store {
 
   Future<void> createDemands(Map<String, dynamic> json) async {
     await _repository.createDemands(json);
-  }
-
-  Future<void> getAllDemands() async {
-    try {
-      var demands = await _repository.getAllDemands();
-
-      demandsList.clear();
-      demandsList.addAll(demands);
-    } on PostgrestException catch (e) {
-      print(e.message);
-    } catch (e) {
-      print(e);
-    }
   }
 
   Future<void> getAllDemandsByUser(String userId) async {
