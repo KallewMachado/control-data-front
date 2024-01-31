6a752caaeacec14b2cf5b2ee70d434ad63b27fc2
Kallew Machado <kallewcarlos@gmail.com>
2024-01-31 15:06:06 -0300

    feat: add BoardDemandsWidget

--- lib/app/modules/initial/views/widgets/board_demands_widget.dart
+++ lib/app/modules/initial/views/widgets/board_demands_widget.dart
@@ -1,15 +1,34 @@
+import 'package:control_data/app/modules/initial/views/initial_store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../../demands/views/widgets/card_demands_widget.dart';
 
 class BoardDemandsWidget extends StatelessWidget {
-  const BoardDemandsWidget({super.key});
+  BoardDemandsWidget({super.key}) {
+    _store.getAllDemands();
+  }
+  final _store = Modular.get<InitialStore>();
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        color: Colors.blue,
-        child: const Center(child: Text('Lista de demandas')),
-      ),
+    return Observer(
+      builder: (context) {
+        return Expanded(
+          child: ListView.builder(
+            itemCount: _store.demandsList.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: CardDemandsWidget(
+                  demand: _store.demandsList[index],
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
