daf042f5ff9d625a3e288152d325323a050451bf
Kallew Machado <kallewcarlos@gmail.com>
2024-01-31 18:00:45 -0300

    feat: add inject dependecy

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -3,8 +3,10 @@ import 'package:control_data/app/modules/auth/views/auth_store.dart';
 import 'package:control_data/app/modules/users/views/users_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'core/repositories/demands_repository.dart';
 import 'core/repositories/user_repository.dart';
 import 'core/store/app_store.dart';
+import 'modules/demands/views/demands_store.dart';
 
 class AppModule extends Module {
   @override
@@ -12,7 +14,9 @@ class AppModule extends Module {
     i.addLazySingleton<AppStore>(AppStore.new);
     i.addLazySingleton<UsersStore>(UsersStore.new);
     i.addLazySingleton<AuthStore>(AuthStore.new);
+    i.addLazySingleton<DemandsStore>(DemandsStore.new);
     i.add<UserRepository>(UserRepositoryImpl.new);
+    i.add<DemandsRepository>(DemandsRepositoryImpl.new);
   }
 
   @override
