03f7a610d82d21c1e108e1b3c9d122c050280816
Kallew Machado <kallewcarlos@gmail.com>
2024-02-01 16:27:50 -0300

    feat: remove Repository

--- lib/app/modules/initial/initial_module.dart
+++ lib/app/modules/initial/initial_module.dart
@@ -1,4 +1,3 @@
-import 'package:control_data/app/core/repositories/demands_repository.dart';
 import 'package:control_data/app/modules/initial/views/initial_page.dart';
 import 'package:control_data/app/modules/initial/views/initial_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -8,7 +7,6 @@ class InitialModule extends Module {
   void binds(i) {
     super.binds(i);
     i.addLazySingleton<InitialStore>(InitialStore.new);
-    i.add<DemandsRepository>(DemandsRepositoryImpl.new);
   }
 
   @override
