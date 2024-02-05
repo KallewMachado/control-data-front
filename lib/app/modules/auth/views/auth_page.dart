24f49c8001996639ee043e22782a91aa0f90748b
Kallew Machado <kallewcarlos@gmail.com>
2024-02-05 11:32:38 -0300

    feat: add check conection

--- lib/app/modules/auth/views/auth_page.dart
+++ lib/app/modules/auth/views/auth_page.dart
@@ -1,9 +1,18 @@
+import 'dart:async';
+import 'dart:developer' as developer;
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:control_data/app/core/model/auth_model.dart';
+import 'package:control_data/app/core/store/app_store.dart';
+import 'package:control_data/app/core/views/widgets/custom_dialog_widget.dart';
 import 'package:control_data/app/core/views/widgets/custom_textform_widget.dart';
+import 'package:control_data/app/core/views/widgets/snackbar_widget.dart';
 import 'package:control_data/app/modules/auth/views/auth_store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthPage extends StatefulWidget {
@@ -15,9 +24,82 @@ class AuthPage extends StatefulWidget {
 
 class _AuthPageState extends State<AuthPage> {
   final _store = Modular.get<AuthStore>();
+  final _appStore = Modular.get<AppStore>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final formKey = GlobalKey<FormState>();
+
+  late StreamSubscription<InternetConnectionStatus> listener;
+
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initConnectivity();
+
+    listener = InternetConnectionChecker().onStatusChange.listen((status) {
+      switch (status) {
+        case InternetConnectionStatus.connected:
+          developer.log(
+            'run [InternetConnectionChecker()] ==> $status',
+            name: 'auth_page.dart',
+          );
+          _appStore.toggleHasInternetStatus(true);
+
+          break;
+        case InternetConnectionStatus.disconnected:
+          developer.log(
+            'run [InternetConnectionChecker()] ==> $status',
+            name: 'auth_page.dart',
+          );
+          _appStore.toggleHasInternetStatus(false);
+          CustomDialogWidet.show(
+            context,
+            content: (context) => const Text(
+                'falha na conexão, verifique sua conexão com a internet e tente novamente'),
+          );
+
+          break;
+      }
+    });
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    listener.cancel();
+    super.dispose();
+  }
+
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
+  }
+
+  checkConnectivity() {}
 
   @override
   Widget build(BuildContext context) {
@@ -87,7 +169,22 @@ class _AuthPageState extends State<AuthPage> {
                         await _store.login(auth);
                         Modular.to.navigate('/home/');
                       } on AuthException catch (e) {
-                        print(e.message);
+                        SnackBarWidget.errorSnackBar(context, e.message);
+                        if (_appStore.hasInternet == false) {
+                          CustomDialogWidet.show(
+                            context,
+                            content: (context) => const Text(
+                                'falha na conexão, verifique sua conexão com a internet e tente novamente'),
+                          );
+                        }
+                      } catch (e) {
+                        if (_appStore.hasInternet == false) {
+                          CustomDialogWidet.show(
+                            context,
+                            content: (context) => const Text(
+                                'falha na conexão, verifique sua conexão com a internet e tente novamente'),
+                          );
+                        }
                       }
                     }
                   },
