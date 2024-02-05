424aa46ab777053749d95c268f79945dc27ca5c2
Kallew Machado <kallewcarlos@gmail.com>
2024-02-05 11:30:32 -0300

    feat: add updateUser

--- lib/app/core/repositories/user_repository.dart
+++ lib/app/core/repositories/user_repository.dart
@@ -8,9 +8,9 @@ abstract class UserRepository {
   Future<UserModel> login(AuthModel auth);
   Future<UserModel> newUser(Map<String, dynamic> json);
   Future<List<UserModel>> getAllUsers();
-  Future<UserModel> updateUser(String json);
+  Future<UserModel> updateUser(Map<String, dynamic> json, String id);
   Future<UserModel> deleteUser(String id);
-  Future<void> logout(String json);
+  Future<void> logout();
 }
 
 class UserRepositoryImpl implements UserRepository {
@@ -35,55 +35,100 @@ class UserRepositoryImpl implements UserRepository {
 
   @override
   Future<List<UserModel>> getAllUsers() async {
-    List<UserModel> users = [];
-    final data = await _supabase.from('users').select();
-    var response = UserModel.fromJsonList(data);
+    try {
+      List<UserModel> users = [];
+      final data = await _supabase.from('users').select();
+      var response = UserModel.fromJsonList(data);
 
-    users.addAll(response);
+      users.addAll(response);
 
-    return users;
+      return users;
+    } on PostgrestException catch (_) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
   Future<UserModel> login(AuthModel auth) async {
-    final AuthResponse res = await _supabase.auth.signInWithPassword(
-      email: auth.email,
-      password: auth.password,
-    );
-    final User? user = res.user;
+    try {
+      final AuthResponse res = await _supabase.auth.signInWithPassword(
+        email: auth.email,
+        password: auth.password,
+      );
+      final User? user = res.user;
 
-    final data = await _supabase.from('users').select().eq('id', '${user?.id}');
+      final data =
+          await _supabase.from('users').select().eq('id', '${user?.id}');
 
-    UserModel? result = UserModel.fromJson(data[0]);
+      UserModel? result = UserModel.fromJson(data[0]);
 
-    return result;
+      return result;
+    } on AuthException catch (_) {
+      rethrow;
+    } on PostgrestException catch (_) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
-  Future<void> logout(String json) {
-    throw UnimplementedError();
+  Future<void> logout() async {
+    try {
+      await _supabase.auth.signOut();
+    } on AuthException catch (_) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
   Future<UserModel> newUser(Map<String, dynamic> json) async {
-    var response = await _supabase.from('users').insert(json).select();
+    try {
+      var response = await _supabase.from('users').insert(json).select();
 
-    UserModel user = userModelFromJson(jsonEncode(response[0]));
+      UserModel user = userModelFromJson(jsonEncode(response[0]));
 
-    return user;
+      return user;
+    } on PostgrestException catch (_) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
-  Future<UserModel> updateUser(String json) {
-    throw UnimplementedError();
+  Future<UserModel> updateUser(Map<String, dynamic> json, String id) async {
+    try {
+      var response =
+          await _supabase.from('users').update(json).eq('id', id).select();
+
+      UserModel user = userModelFromJson(jsonEncode(response[0]));
+
+      return user;
+    } on PostgrestException catch (_) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
   Future<UserModel> deleteUser(String id) async {
-    var response = await _supabase.from('users').delete().eq('id', id).select();
+    try {
+      var response =
+          await _supabase.from('users').delete().eq('id', id).select();
 
-    UserModel user = userModelFromJson(jsonEncode(response[0]));
+      UserModel user = userModelFromJson(jsonEncode(response[0]));
 
-    return user;
+      return user;
+    } on PostgrestException catch (_) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
   }
 }
