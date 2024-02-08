2ea87a7e4d2ff8fe8406210afa3cab86a6242cf5
Kallew Machado <kallewcarlos@gmail.com>
2024-02-08 10:26:40 -0300

    feat: add _deleteUser

--- lib/app/modules/users/views/widgets/card_user_widget.dart
+++ lib/app/modules/users/views/widgets/card_user_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:developer' as developer;
+import 'package:control_data/app/core/store/app_store.dart';
 import 'package:control_data/app/modules/users/views/users_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -5,6 +7,7 @@ import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../../core/model/user_model.dart';
+import '../../../../core/views/widgets/custom_dialog_widget.dart';
 import '../../../../core/views/widgets/snackbar_widget.dart';
 
 class CardUserWidget extends StatefulWidget {
@@ -17,9 +20,59 @@ class CardUserWidget extends StatefulWidget {
 }
 
 class _CardUserWidgetState extends State<CardUserWidget> {
+  _deleteUser() {
+    developer.log(
+      'run [Delete User] ==> ',
+      name: 'auth_page.dart',
+    );
+    CustomDialogWidet.show(
+      context,
+      title: (context) =>
+          const Text(' Tem certeza que deseja excluir usuario?'),
+      content: (context) => const SizedBox(height: 0),
+      actions: [
+        ElevatedButton(
+          onPressed: () {
+            Modular.to.pop();
+          },
+          child: const Text('Não'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            try {
+              await widget.store.deleteUser(widget.user.id);
+              if (mounted) {
+                SnackBarWidget.successSnackBar(
+                    context, 'Usuário excluido com sucesso!');
+              }
+              Modular.to.pop();
+            } on PostgrestException catch (e) {
+              Modular.to.pop();
+              SnackBarWidget.errorSnackBar(context, e.message);
+            } catch (e) {
+              Modular.to.pop();
+              if (appStore.hasInternet == false) {
+                CustomDialogWidet.show(
+                  context,
+                  content: (context) => const Text(
+                      'falha na conexão, verifique sua conexão com a internet e tente novamente'),
+                );
+              } else {
+                SnackBarWidget.errorSnackBar(context, e.toString());
+              }
+            }
+          },
+          child: const Text('Sim'),
+        ),
+      ],
+    );
+  }
+
+  final appStore = Modular.get<AppStore>();
   @override
   Widget build(BuildContext context) {
     DateTime dateBirth = widget.user.dateBirth;
+    var userbox = widget.store.hive.userBox.values.first;
     var date = DateFormat('dd/MM/yyyy');
     return Card(
       elevation: 5,
@@ -31,27 +84,13 @@ class _CardUserWidgetState extends State<CardUserWidget> {
         leading: const Icon(Icons.person),
         title: Text(widget.user.name),
         subtitle: Text(date.format(dateBirth)),
-        trailing: IconButton(
-          onPressed: () async {
-            try {
-              await widget.store.deleteUser(widget.user.id);
-              if (mounted) {
-                SnackBarWidget.successSnackBar(
-                    context, 'Usuário excluido com sucesso!');
-              }
-            } on PostgrestException catch (e) {
-              if (mounted) {
-                SnackBarWidget.errorSnackBar(context, e.message);
-              }
-            } catch (e) {
-              if (mounted) {
-                SnackBarWidget.errorSnackBar(context, e.toString());
-              }
-            }
-          },
-          icon: const Icon(Icons.delete),
-          color: Colors.red,
-        ),
+        trailing: widget.user.id != userbox.id
+            ? IconButton(
+                onPressed: _deleteUser,
+                icon: const Icon(Icons.delete),
+                color: Colors.red,
+              )
+            : null,
       ),
     );
   }
