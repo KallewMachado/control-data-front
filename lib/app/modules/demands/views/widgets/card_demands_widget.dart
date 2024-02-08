d9b2b63159d38d2d5d6609ede8c63b3e334d0762
Kallew Machado <kallewcarlos@gmail.com>
2024-02-08 10:25:58 -0300

    feat: add _deleteDemand

--- lib/app/modules/demands/views/widgets/card_demands_widget.dart
+++ lib/app/modules/demands/views/widgets/card_demands_widget.dart
@@ -1,20 +1,80 @@
+import 'dart:developer' as developer;
 import 'package:control_data/app/core/model/demands_model.dart';
+import 'package:control_data/app/core/store/app_store.dart';
 import 'package:control_data/app/core/views/widgets/snackbar_widget.dart';
 import 'package:control_data/app/modules/demands/views/demands_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../../../../core/views/widgets/custom_dialog_widget.dart';
+
 class CardDemandsWidget extends StatefulWidget {
-  const CardDemandsWidget({super.key, required this.demand});
+  const CardDemandsWidget(
+      {super.key, required this.demand, required this.store});
   final DemandsModel demand;
+  final DemandsStore store;
 
   @override
   State<CardDemandsWidget> createState() => _CardDemandsWidgetState();
 }
 
 class _CardDemandsWidgetState extends State<CardDemandsWidget> {
-  final _store = Modular.get<DemandsStore>();
+  final appStore = Modular.get<AppStore>();
+
+  _deleteDemand() {
+    developer.log(
+      'run [Delete Demand] ==> ',
+      name: 'card_demands_widget.dart',
+    );
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) {
+        CustomDialogWidet.show(
+          context,
+          title: (context) =>
+              const Text(' Tem certeza que deseja excluir usuario?'),
+          content: (context) => const SizedBox(height: 0),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Modular.to.pop();
+              },
+              child: const Text('Não'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  await widget.store.removeDemand(widget.demand.id);
+                  await widget.store.getAllDemandsByUser(widget.demand.userId);
+
+                  if (mounted) {
+                    SnackBarWidget.successSnackBar(
+                        context, 'Demanda excluida com sucesso!');
+                  }
+                  Modular.to.pop();
+                } on PostgrestException catch (e) {
+                  Modular.to.pop();
+                  SnackBarWidget.errorSnackBar(context, e.message);
+                } catch (e) {
+                  Modular.to.pop();
+                  if (appStore.hasInternet == false) {
+                    CustomDialogWidet.show(
+                      context,
+                      content: (context) => const Text(
+                          'falha na conexão, verifique sua conexão com a internet e tente novamente'),
+                    );
+                  } else {
+                    SnackBarWidget.errorSnackBar(context, e.toString());
+                  }
+                }
+              },
+              child: const Text('Sim'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,8 +107,8 @@ class _CardDemandsWidgetState extends State<CardDemandsWidget> {
                     "done_date": dateDone
                   };
 
-                  await _store.updateDemands(json, widget.demand.id);
-                  await _store.getAllDemandsByUser(widget.demand.userId);
+                  await widget.store.updateDemands(json, widget.demand.id);
+                  await widget.store.getAllDemandsByUser(widget.demand.userId);
 
                   if (mounted) {
                     SnackBarWidget.successSnackBar(
@@ -72,25 +132,7 @@ class _CardDemandsWidgetState extends State<CardDemandsWidget> {
               child: const Text('Editar'),
             ),
             PopupMenuItem(
-              onTap: () async {
-                try {
-                  await _store.removeDemand(widget.demand.id);
-                  await _store.getAllDemandsByUser(widget.demand.userId);
-
-                  if (mounted) {
-                    SnackBarWidget.successSnackBar(
-                        context, 'Demanda excluida com sucesso!');
-                  }
-                } on PostgrestException catch (e) {
-                  if (mounted) {
-                    SnackBarWidget.errorSnackBar(context, e.message);
-                  }
-                } catch (e) {
-                  if (mounted) {
-                    SnackBarWidget.errorSnackBar(context, e.toString());
-                  }
-                }
-              },
+              onTap: _deleteDemand,
               enabled: widget.demand.isDone == true ? false : true,
               child: const Text('Remover'),
             ),
