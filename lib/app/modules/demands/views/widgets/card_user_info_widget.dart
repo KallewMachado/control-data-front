127c4d497d7a9b4bacbd7d2c0075f0c4c372d261
Kallew Machado <kallewcarlos@gmail.com>
2024-05-20 11:56:40 -0300

    fix: remove import

--- lib/app/modules/demands/views/widgets/card_user_info_widget.dart
+++ lib/app/modules/demands/views/widgets/card_user_info_widget.dart
@@ -1,7 +1,6 @@
 import 'package:control_data/app/core/model/user_model.dart';
 import 'package:control_data/app/core/utils/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/intl.dart';
 
