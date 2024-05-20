ca51bc8f5411bb15a96aa23e623fde760e96f99c
Kallew Machado <kallewcarlos@gmail.com>
2024-05-20 11:54:00 -0300

    fix: remove observer

--- lib/app/modules/demands/views/widgets/card_user_info_widget.dart
+++ lib/app/modules/demands/views/widgets/card_user_info_widget.dart
@@ -1,4 +1,5 @@
 import 'package:control_data/app/core/model/user_model.dart';
+import 'package:control_data/app/core/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -19,53 +20,49 @@ class _CardUserInfoWidgetState extends State<CardUserInfoWidget> {
     var user = widget.user;
     var date = DateFormat('dd/MM/yyyy');
 
-    return Observer(
-      builder: (context) {
-        return Card(
-          elevation: 5,
-          child: Container(
-            padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: theme.primary.withOpacity(0.2)),
-            child: Row(
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: theme.primary.withOpacity(0.2)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                        '${user.name.split(' ').first} ${user.name.split(' ').last}'),
-                    Text(user.email),
-                    Text(user.cpf ?? ''),
-                    Text(user.street),
-                    Text(user.district),
-                  ],
-                ),
-                const Spacer(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                        '${(DateTime.now().difference(user.dateBirth).inDays / 365).round()} anos'),
-                    Text(user.fone),
-                    Text(date.format(user.dateBirth)),
-                    Text(user.num),
-                    Text(user.complement ?? '')
-                  ],
-                ),
-                const Spacer(),
-                IconButton(
-                  onPressed: () {
-                    Modular.to.pushNamed('/changeUser', arguments: user);
-                  },
-                  icon: const Icon(Icons.edit),
-                ),
+                Text(
+                    '${user.name.split(' ').first} ${user.name.split(' ').last}'),
+                Text(user.email),
+                Text(user.cpf ?? ''),
+                Text(user.street),
+                Text(user.district),
               ],
             ),
-          ),
-        );
-      },
+            const Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                    '${(DateTime.now().difference(user.dateBirth).inDays / 365).round()} anos'),
+                Text(user.fone),
+                Text(date.format(user.dateBirth)),
+                Text(user.num),
+                Text(user.complement ?? '')
+              ],
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {
+                Modular.to.pushNamed(routesPath.changeUser, arguments: user);
+              },
+              icon: const Icon(Icons.edit),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
