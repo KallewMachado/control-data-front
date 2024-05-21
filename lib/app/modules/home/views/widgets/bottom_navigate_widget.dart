679542242327e4748af012f226944076532c919b
Kallew Machado <kallewcarlos@gmail.com>
2024-05-21 15:28:37 -0300

    refactor: listPages

--- lib/app/modules/home/views/widgets/bottom_navigate_widget.dart
+++ lib/app/modules/home/views/widgets/bottom_navigate_widget.dart
@@ -1,37 +1,39 @@
+import 'package:control_data/app/modules/home/views/home_store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../../../core/utils/enum.dart';
 import '../../../../core/utils/routes.dart';
-import '../home_page.dart';
 
 class BottomNavigateWidget extends StatelessWidget {
-  const BottomNavigateWidget({super.key, required this.selection});
-  final Set<Pages> selection;
+  const BottomNavigateWidget({super.key, required this.store});
+
+  final HomeStore store;
 
   @override
   Widget build(BuildContext context) {
-    return SegmentedButton<Pages>(
-      style: ButtonStyle(
-        side: MaterialStateProperty.all(
-          const BorderSide(color: Colors.transparent),
-        ),
-      ),
-      showSelectedIcon: false,
-      segments: <ButtonSegment<Pages>>[
-        ButtonSegment<Pages>(
-            value: Pages.initial, label: Text(Pages.initial.title)),
-        ButtonSegment<Pages>(
-            value: Pages.users, label: Text(Pages.users.title)),
-        ButtonSegment<Pages>(
-            value: Pages.config, label: Text(Pages.config.title)),
-      ],
-      selected: selection,
-      onSelectionChanged: (Set<Pages> newSelection) {
-        var router = newSelection.first.name;
+    return Observer(builder: (context) {
+      return SegmentedButton<Pages>(
+        showSelectedIcon: false,
+        segments: _listPages,
+        selected: store.selection,
+        onSelectionChanged: onSelectionChanged,
+        multiSelectionEnabled: false,
+      );
+    });
+  }
 
-        Modular.to.navigate('${routesPath.auth.home}$router/');
-      },
-      multiSelectionEnabled: false,
-    );
+  onSelectionChanged(Set<Pages> newSelection) {
+    var router = newSelection.first.name;
+    store.changeSelectionPage(newSelection);
+
+    Modular.to.navigate('${routesPath.auth.home}$router/');
   }
 }
+
+var _listPages = <ButtonSegment<Pages>>[
+  ButtonSegment<Pages>(value: Pages.initial, label: Text(Pages.initial.title)),
+  ButtonSegment<Pages>(value: Pages.users, label: Text(Pages.users.title)),
+  ButtonSegment<Pages>(value: Pages.config, label: Text(Pages.config.title)),
+];
